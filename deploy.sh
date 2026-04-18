ac813f0ef55936a1b55e249252381bfa1f65e1cd
zainab <errossafizainab91@gmail.com>
2026-04-18 21:29:33 +0100

    fix cert path

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://192.168.49.2:2376"
-export DOCKER_CERT_PATH="/home/zainab/.minikube/certs"
+export DOCKER_CERT_PATH="/var/lib/jenkins/.minikube/certs"
 export MINIKUBE_ACTIVE_DOCKERD="minikube"
 cd /var/lib/jenkins/workspace/devops-pipeline
 docker build -t webapp:latest .
