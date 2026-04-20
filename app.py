e4acbd58a000f28c39a9ec9708083df13bf2c96d
zainab <errossafizainab91@gmail.com>
2026-04-20 21:08:47 +0100

    test2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version 11 - Deployee automatiquement"
+    return "Version 12 - Deployee automatiquement"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
